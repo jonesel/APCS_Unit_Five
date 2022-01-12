d98cc394cd8a8f49e733fe6afee2b5b12f8d8046
wilkinsonbrian <wilkibrian@gmail.com>
2022-01-12 10:09:37 -0500

    Updated toString tests for Line

--- src/Line.java
+++ src/Line.java
@@ -1,27 +1,4 @@
 class Line {
 
-    private int a, b, c;
-
-    public Line(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public double getSlope() {
-        return (double)a/b * -1;
-    }
-
-    public boolean isOnLine(int x, int y) {
-        return a*x + b*y + c == 0;
-    }
-
-    public boolean isParallel(Line other) {
-        return this.getSlope() == other.getSlope();
-    }
-
-    public String toString() {
-        return a + "x + " + b + "y + " + c + " = 0";
-    }
 
 }
